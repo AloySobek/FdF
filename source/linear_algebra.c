16ae0f03b7e0a9599264ce4c7c4faaad9b81a3cd
Vince Richese <vrichese@oa-o1.21-school.ru>
2019-07-16 21:30:11 +0300

    In linear_algebra file some optimization multiply

--- source/linear_algebra.c
+++ source/linear_algebra.c
@@ -6,7 +6,7 @@
 /*   By: vrichese <vrichese@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/04 19:11:46 by vrichese          #+#    #+#             */
-/*   Updated: 2019/07/14 16:36:19 by vrichese         ###   ########.fr       */
+/*   Updated: 2019/07/16 21:28:21 by vrichese         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,27 +32,21 @@ void	x_y_z_axis_rotation(t_mlx_var *mlx_var)
 int		scalar_product_of_vectors(t_mlx_var *mlx_var)
 {
 	mlx_var->linear_algebra.vectors.x =
-		(mlx_var->maps->x * mlx_var->linear_algebra.basis_vectors[X][X]
-		* mlx_var->screen.scale)
-		+ (mlx_var->maps->y * mlx_var->linear_algebra.basis_vectors[Y][X]
-		* mlx_var->screen.scale)
+		((mlx_var->maps->x * mlx_var->linear_algebra.basis_vectors[X][X])
+		+ (mlx_var->maps->y * mlx_var->linear_algebra.basis_vectors[Y][X])
 		+ (mlx_var->maps->z * mlx_var->linear_algebra.basis_vectors[Z][X]
-		* mlx_var->screen.altitude * mlx_var->screen.scale)
+		* mlx_var->screen.altitude)) * mlx_var->screen.scale
 		+ mlx_var->linear_algebra.horizontal;
 	mlx_var->linear_algebra.vectors.y =
-		(mlx_var->maps->x * mlx_var->linear_algebra.basis_vectors[X][Y]
-		* mlx_var->screen.scale)
-		+ (mlx_var->maps->y * mlx_var->linear_algebra.basis_vectors[Y][Y]
-		* mlx_var->screen.scale)
+		((mlx_var->maps->x * mlx_var->linear_algebra.basis_vectors[X][Y])
+		+ (mlx_var->maps->y * mlx_var->linear_algebra.basis_vectors[Y][Y])
 		+ (mlx_var->maps->z * mlx_var->linear_algebra.basis_vectors[Z][Y]
-		* mlx_var->screen.altitude * mlx_var->screen.scale)
+		* mlx_var->screen.altitude)) * mlx_var->screen.scale
 		+ mlx_var->linear_algebra.vertical;
 	mlx_var->linear_algebra.vectors.z =
-		((mlx_var->maps->x * mlx_var->linear_algebra.basis_vectors[X][Z]
-		* mlx_var->screen.scale)
-		+ (mlx_var->maps->y * mlx_var->linear_algebra.basis_vectors[Y][Z]
-		* mlx_var->screen.scale)
+		((mlx_var->maps->x * mlx_var->linear_algebra.basis_vectors[X][Z])
+		+ (mlx_var->maps->y * mlx_var->linear_algebra.basis_vectors[Y][Z])
 		+ (mlx_var->maps->z * mlx_var->linear_algebra.basis_vectors[Z][Z]
-		* mlx_var->screen.altitude * mlx_var->screen.scale));
+		* mlx_var->screen.altitude)) * mlx_var->screen.scale;
 	return (1);
 }
